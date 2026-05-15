8bf0fe901e4764e6cc81459d6aa3cf1c11acc129
Yuh4D <dinhhuy11300@gmail.com>
2026-05-16 04:15:58 +0700

    update code

--- WebQLPT/Controllers/HoaDonsController.cs
+++ WebQLPT/Controllers/HoaDonsController.cs
@@ -7,8 +7,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Security.Claims;
 using WebQLPT.Data;
 using WebQLPT.Models;
+using WebQLPT.Helpers;
 
 namespace WebQLPT.Controllers
 {
@@ -25,8 +27,41 @@ namespace WebQLPT.Controllers
         // GET: HoaDons
         public async Task<IActionResult> Index()
         {
-            var appDbContext = _context.HoaDons.Include(h => h.KhachThue).Include(h => h.PhongTro);
-            return View(await appDbContext.ToListAsync());
+            var query = _context.HoaDons
+                .Include(h => h.KhachThue)
+                .Include(h => h.PhongTro)
+                    .ThenInclude(p => p.ChuTro)
+                .AsQueryable();
+
+      
+            if (User.IsInRole("admin"))
+            {
+                return View(await query.ToListAsync());
+            }
+
+      
+            if (User.IsInRole("chutro"))
+            {
+                var chuTroId = UserHelper.GetChuTroId(User);
+
+                query = query.Where(h =>
+                    h.PhongTro.ChuTroId == chuTroId);
+
+                return View(await query.ToListAsync());
+            }
+
+         
+            if (User.IsInRole("khachthue"))
+            {
+                var khachThueId = UserHelper.GetKhachThueId(User);
+
+                query = query.Where(h =>
+                    h.KhachThueId == khachThueId);
+
+                return View(await query.ToListAsync());
+            }
+
+            return Forbid();
         }
 
         // GET: HoaDons/Details/5
@@ -43,50 +78,134 @@ namespace WebQLPT.Controllers
                     .ThenInclude(p => p.ChuTro)
                 .Include(h => h.HoaDonChiTiets)
                 .FirstOrDefaultAsync(m => m.Id == id);
+
             if (hoaDon == null)
             {
                 return NotFound();
             }
 
-            return View(hoaDon);
+        
+            if (User.IsInRole("admin"))
+            {
+                return View(hoaDon);
+            }
+
+            if (User.IsInRole("chutro"))
+            {
+                var chuTroId = UserHelper.GetChuTroId(User);
+
+                if (hoaDon.PhongTro.ChuTroId != chuTroId)
+                {
+                    return Forbid();
+                }
+
+                return View(hoaDon);
+            }
+
+            
+            if (User.IsInRole("khachthue"))
+            {
+                var khachThueId = UserHelper.GetKhachThueId(User);
+
+                if (hoaDon.KhachThueId != khachThueId)
+                {
+                    return Forbid();
+                }
+
+                return View(hoaDon);
+            }
+
+            return Forbid();
         }
 
         // GET: HoaDons/Create
+        [Authorize(Roles = "admin,chutro")]
         public IActionResult Create()
         {
-            ViewData["PhongTroId"] = new SelectList(_context.PhongTros, "Id", "TenPhong");
+            // ADMIN
+            if (User.IsInRole("admin"))
+            {
+                ViewData["PhongTroId"] =
+                    new SelectList(_context.PhongTros,
+                        "Id",
+                        "TenPhong");
+
+                return View();
+            }
+
+          
+            var chuTroId = UserHelper.GetChuTroId(User);
+
+            var phongTroList = _context.PhongTros
+                .Where(p => p.ChuTroId == chuTroId)
+                .ToList();
+
+            ViewData["PhongTroId"] =
+                new SelectList(phongTroList,
+                    "Id",
+                    "TenPhong");
+
             return View();
         }
 
         // POST: HoaDons/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize(Roles = "admin,chutro")]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(HoaDon hoaDon)
         {
-            if (!ModelState.IsValid)
+            // Kiểm tra phòng tồn tại
+            var phongTro = await _context.PhongTros
+                .FirstOrDefaultAsync(p => p.Id == hoaDon.PhongTroId);
+
+            if (phongTro == null)
             {
-                ViewData["PhongTroId"] = new SelectList(_context.PhongTros, "Id", "TenPhong", hoaDon.PhongTroId);
-                return View(hoaDon);
-       
+                return NotFound();
             }
 
-            //Lấy khách theo phòng
+            // CHỦ TRỌ chỉ được tạo hóa đơn phòng mình
+            if (User.IsInRole("chutro"))
+            {
+                var chuTroId = UserHelper.GetChuTroId(User);
+
+                if (phongTro.ChuTroId != chuTroId)
+                {
+                    return Forbid();
+                }
+            }
+
+            if (!ModelState.IsValid)
+            {
+                ViewData["PhongTroId"] =
+                    new SelectList(_context.PhongTros,
+                        "Id",
+                        "TenPhong",
+                        hoaDon.PhongTroId);
+
+                return View(hoaDon);
+            }
+
+            // Lấy khách thuê theo phòng
             var khach = await _context.KhachThues
-                .FirstOrDefaultAsync(k => k.PhongTroId == hoaDon.PhongTroId);
+                .FirstOrDefaultAsync(k =>
+                    k.PhongTroId == hoaDon.PhongTroId);
 
             if (khach == null)
             {
                 TempData["Error"] = "Phòng chưa có khách thuê!";
+
                 return RedirectToAction(nameof(Create));
             }
 
-            //Gán dữ liệu tự động
+            // Gán tự động
             hoaDon.KhachThueId = khach.Id;
+
             hoaDon.TrangThai = "Mới tạo";
 
             _context.Add(hoaDon);
+
             await _context.SaveChangesAsync();
 
             decimal tong = 0;
@@ -95,13 +214,18 @@ namespace WebQLPT.Controllers
             {
                 foreach (var ct in hoaDon.ChiTiets)
                 {
-                    if (string.IsNullOrEmpty(ct.NoiDung)) continue;
+                    if (string.IsNullOrEmpty(ct.NoiDung))
+                        continue;
 
                     ct.HoaDonId = hoaDon.Id;
 
-                    if (ct.HeSo == 0) ct.HeSo = 1;
+                    if (ct.HeSo == 0)
+                        ct.HeSo = 1;
 
-                    ct.ThanhTien = ct.DonGia * ct.SoLuong * ct.HeSo;
+                    ct.ThanhTien =
+                        ct.DonGia *
+                        ct.SoLuong *
+                        ct.HeSo;
 
                     tong += ct.ThanhTien;
 
@@ -113,10 +237,13 @@ namespace WebQLPT.Controllers
 
             await _context.SaveChangesAsync();
 
-            return RedirectToAction("Details", new { id = hoaDon.Id });
+            return RedirectToAction(
+                "Details",
+                new { id = hoaDon.Id });
         }
 
         // GET: HoaDons/Edit/5
+        [Authorize(Roles = "admin,chutro")]
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -124,26 +251,74 @@ namespace WebQLPT.Controllers
                 return NotFound();
             }
 
-            var hoaDon = await _context.HoaDons.FindAsync(id);
+            var hoaDon = await _context.HoaDons
+                .Include(h => h.PhongTro)
+                .FirstOrDefaultAsync(h => h.Id == id);
+
             if (hoaDon == null)
             {
                 return NotFound();
             }
-            ViewData["KhachThueId"] = new SelectList(_context.KhachThues, "Id", "Id", hoaDon.KhachThueId);
-            ViewData["PhongTroId"] = new SelectList(_context.PhongTros, "Id", "Id", hoaDon.PhongTroId);
+
+   
+            if (User.IsInRole("chutro"))
+            {
+                var chuTroId = UserHelper.GetChuTroId(User);
+
+                if (hoaDon.PhongTro.ChuTroId != chuTroId)
+                {
+                    return Forbid();
+                }
+            }
+
+            ViewData["KhachThueId"] =
+                new SelectList(_context.KhachThues,
+                    "Id",
+                    "Id",
+                    hoaDon.KhachThueId);
+
+            ViewData["PhongTroId"] =
+                new SelectList(_context.PhongTros,
+                    "Id",
+                    "TenPhong",
+                    hoaDon.PhongTroId);
+
             return View(hoaDon);
         }
 
         // POST: HoaDons/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize(Roles = "admin,chutro")]
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,PhongTroId,KhachThueId,NgayTao,HanThanhToan,TongTien,TrangThai")] HoaDon hoaDon)
+        public async Task<IActionResult> Edit(int id,
+            [Bind("Id,PhongTroId,KhachThueId,NgayTao,HanThanhToan,TongTien,TrangThai")]
+            HoaDon hoaDon)
         {
             if (id != hoaDon.Id)
             {
                 return NotFound();
+            }
+
+            var oldHoaDon = await _context.HoaDons
+                .Include(h => h.PhongTro)
+                .FirstOrDefaultAsync(h => h.Id == id);
+
+            if (oldHoaDon == null)
+            {
+                return NotFound();
+            }
+
+          
+            if (User.IsInRole("chutro"))
+            {
+                var chuTroId = UserHelper.GetChuTroId(User);
+
+                if (oldHoaDon.PhongTro.ChuTroId != chuTroId)
+                {
+                    return Forbid();
+                }
             }
 
             if (ModelState.IsValid)
@@ -151,6 +326,7 @@ namespace WebQLPT.Controllers
                 try
                 {
                     _context.Update(hoaDon);
+
                     await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
@@ -159,19 +335,18 @@ namespace WebQLPT.Controllers
                     {
                         return NotFound();
                     }
-                    else
-                    {
-                        throw;
-                    }
+
+                    throw;
                 }
+
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["KhachThueId"] = new SelectList(_context.KhachThues, "Id", "Id", hoaDon.KhachThueId);
-            ViewData["PhongTroId"] = new SelectList(_context.PhongTros, "Id", "Id", hoaDon.PhongTroId);
+
             return View(hoaDon);
         }
 
         // GET: HoaDons/Delete/5
+        [Authorize(Roles = "admin,chutro")]
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -183,26 +358,56 @@ namespace WebQLPT.Controllers
                 .Include(h => h.KhachThue)
                 .Include(h => h.PhongTro)
                 .FirstOrDefaultAsync(m => m.Id == id);
+
             if (hoaDon == null)
             {
                 return NotFound();
+            }
+
+        
+            if (User.IsInRole("chutro"))
+            {
+                var chuTroId = UserHelper.GetChuTroId(User);
+
+                if (hoaDon.PhongTro.ChuTroId != chuTroId)
+                {
+                    return Forbid();
+                }
             }
 
             return View(hoaDon);
         }
 
         // POST: HoaDons/Delete/5
+        [Authorize(Roles = "admin,chutro")]
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var hoaDon = await _context.HoaDons.FindAsync(id);
-            if (hoaDon != null)
+            var hoaDon = await _context.HoaDons
+                .Include(h => h.PhongTro)
+                .FirstOrDefaultAsync(h => h.Id == id);
+
+            if (hoaDon == null)
             {
-                _context.HoaDons.Remove(hoaDon);
+                return NotFound();
             }
 
+     
+            if (User.IsInRole("chutro"))
+            {
+                var chuTroId = UserHelper.GetChuTroId(User);
+
+                if (hoaDon.PhongTro.ChuTroId != chuTroId)
+                {
+                    return Forbid();
+                }
+            }
+
+            _context.HoaDons.Remove(hoaDon);
+
             await _context.SaveChangesAsync();
+
             return RedirectToAction(nameof(Index));
         }
 
@@ -221,38 +426,6 @@ namespace WebQLPT.Controllers
             return View();
         }
 
-        // POST : HoaDons/AddDetail
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> AddDetail([Bind("HoaDonId,NoiDung,DonGia,SoLuong,HeSo")] HoaDonChiTiet ct)
-        //{
-        //    if (!ModelState.IsValid)
-        //    {
-        //        ViewBag.HoaDonId = ct.HoaDonId;
-        //        return View(ct);
-
-        //    }
-
-        //    if (ct.HeSo == 0)
-        //        ct.HeSo = 1;
-
-        //    ct.ThanhTien = ct.DonGia * ct.SoLuong * ct.HeSo;
-
-        //    _context.HoaDonChiTiets.Add(ct);
-        //    await _context.SaveChangesAsync();
-
-        //    var tongTien = await _context.HoaDonChiTiets
-        //        .Where(x => x.HoaDonId == ct.HoaDonId)
-        //        .SumAsync(x => x.ThanhTien);
-
-        //    var hoaDon = await _context.HoaDons.FindAsync(ct.HoaDonId);
-        //    hoaDon.TongTien = tongTien;
-
-        //    _context.Update(hoaDon);
-        //    await _context.SaveChangesAsync();
-
-        //    return RedirectToAction(nameof(Details), new { id = ct.HoaDonId });
-        //}
 
         public async Task<IActionResult> ExportPdf(int id)
         {
@@ -262,6 +435,32 @@ namespace WebQLPT.Controllers
                 .Include(h => h.KhachThue)
                 .Include(h => h.HoaDonChiTiets)
                 .FirstOrDefaultAsync(h => h.Id == id);
+
+            if (hoaDon == null)
+            {
+                return NotFound();
+            }
+
+     
+            if (User.IsInRole("chutro"))
+            {
+                var chuTroId = UserHelper.GetChuTroId(User);
+
+                if (hoaDon.PhongTro.ChuTroId != chuTroId)
+                {
+                    return Forbid();
+                }
+            }
+
+            if (User.IsInRole("khachthue"))
+            {
+                var khachThueId = UserHelper.GetKhachThueId(User);
+
+                if (hoaDon.KhachThueId != khachThueId)
+                {
+                    return Forbid();
+                }
+            }
 
             return new ViewAsPdf("Pdf", hoaDon)
             {
